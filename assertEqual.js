6edf5244659d5ee3854e5be01a8494120bba94ca
zachmanac <zachstephenson94@gmail.com>
2022-09-20 03:55:04 +0000

    Finished function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log("✅️ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    return console.log("❌️ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("Bootcamp", "Bootcamp");
