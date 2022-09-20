0deb44a961b3e0573a95380aefa6e220e6c1c8f5
zachmanac <zachstephenson94@gmail.com>
2022-09-20 04:03:38 +0000

    template literals update

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return console.log("✅️ Assertion Passed: " + actual + " === " + expected);
+    return console.log(`✅️ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    return console.log("❌️ Assertion Failed: " + actual + " !== " + expected);
+    return console.log(`❌️ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
