85cfa19aa9a4359583336a8123fbf68156331baf
zachmanac <zachstephenson94@gmail.com>
2022-09-26 23:31:00 +0000

    refactored head and assertequals

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
-assertEqual("Bootcamp", "Bootcamp");
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,14 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`❌️ Assertion Failed: ${actual} !== ${expected}`);
-  }
+let head = function(arr) {
+  return arr[0];
 };
 
-let head = function(assertEqual) {
-  return assertEqual[0];
-};
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
